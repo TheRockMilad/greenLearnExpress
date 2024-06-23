d26e49a9bd7430c848300de091ad174f17b965d1
TheRockMilad <therock.milad2@outlook.com>
2024-06-23 15:52:00 +0330

    all response

--- app.js
+++ app.js
@@ -17,20 +17,37 @@ const courses = [
     price: 4500000,
   },
 ];
+//-------------- get request --------------------
+app.get("/courses1/:id", (req, res) => {
+  const course = courses.find(
+    (courses) => courses.id === Number(req.params.id)
+  );
+  if (course) {
+    res.send(`<h1>Name course is ${course.title}</h1>`);
+  }
+  res.send("<h1>Course not exist</h1>");
+});
 
+//--------------- main page -------------------
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//-------------- all response ------------------
+app.get("/courses2/:id", (req, res) => {
+  const course = courses.find(
+    (courses) => courses.id === Number(req.params.id)
+  );
+  res.send(courses);
+  //   res.send('Course test')
+  //   res.send("<H1>Course test in html</h1>")
+  //   res.json(courses)
+  //   res.json("test")
+  //   res.end("test course with end") دیگه استفاده نمیشه
+});
+
+//--------------server---------------------------
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
-});
-
-app.get('/courses/:id',(req,res)=>{
-    const course = courses.find(courses => (courses.id === Number(req.params.id)))
-    if (course) {
-        res.send(`Name course is ${course.title}`)
-    }
-    res.send("Course not exist");
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
 });
