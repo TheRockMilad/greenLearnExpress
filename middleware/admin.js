08e99634df53662c9613f11f8576d604cfda0cd4
TheRockMilad <therock.milad2@outlook.com>
2024-06-24 17:18:45 +0330

    Middleware for isAdmin

---
+++ middleware/admin.js
@@ -0,0 +1,30 @@
+const UserModel = require("./../models/users");
+const mongoose = require("mongoose");
+
+async function isAdmin(req, res, next) {
+  const { id } = req.body;
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(433).json({
+      message: "Id is not valid",
+    });
+  } else {
+    const user = await UserModel.findOne({_id : id}).lean()
+    if (user) {
+      if (user.role === "ADMIN") {
+        return next();
+      } else {
+        return res.status(403).json({
+          message: "this route is not accessible only for admin",
+        });
+      }
+    } else {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+  }
+}
+
+module.exports = {
+  isAdmin,
+};
--- router/books.js
+++ router/books.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const booksRouter = express.Router();
 const booksController = require("./../controller/books");
+const {isAdmin} = require('./../middleware/admin')
 
 booksRouter
   .route("/:id?")
-  .get(booksController.getUser)
+  .get(isAdmin,booksController.getUser)
 
 
 module.exports = booksRouter;
