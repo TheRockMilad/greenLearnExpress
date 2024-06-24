9ce56f447c55074c1271b30885d21b9b28ccf34e
TheRockMilad <therock.milad2@outlook.com>
2024-06-25 02:49:59 +0330

    helmet

--- app.js
+++ app.js
@@ -3,33 +3,12 @@ const app = express();
 require("./configs/db");
 const mainRouter = require("./router/main");
 const ApiRouter = require("./router/api");
-const { default: omitEmpty } = require("omit-empty");
+const helmet = require('helmet')
 //----------------------------------------------------------
-
-const removeEmptyFields = (options) => {
-  return function (req, res, next) {
-    req.body = omitEmpty(req.body, options);
-    return next();
-  };
-};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//فراخوانی 
-app.use(removeEmptyFields({omitZero : true}));
-
-// خالی هارو پاک میکنه
-console.log(
-  omitEmpty(
-    {
-      firstname: "",
-      lastname: "",
-      age: 13,
-      email: "dfe@gmail.com",
-    },
-    { omitZero: true }
-  )
-);
+app.use(helmet())
 
 //----------------------------------------------
 
