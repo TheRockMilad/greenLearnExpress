f846cf21fe933c96bdca88e9ada3be0144dcfed0
TheRockMilad <therock.milad2@outlook.com>
2024-06-25 15:48:23 +0330

    add express.static = public

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")))
 app.use(helmet())
 app.use(cors())
 
