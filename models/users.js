644ca0efb03bc8adbebe4e6d851b128fbb22c7e1
TheRockMilad <therock.milad2@outlook.com>
2024-06-25 13:36:33 +0330

    some Change in database & validation

--- models/users.js
+++ models/users.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, minLenth: 3, maxLenth: 12 },
   name: { type: String, required: true, minLenth: 5, maxLenth: 15 },
   email: { type: String, required: true, unique: true },
+  age: { type: Number },
   password: { type: String, minLenth: 8, maxLenth: 12 },
 });
 
--- validator/register.js
+++ validator/register.js
@@ -4,9 +4,10 @@ const Schema = {
   name: { type: "string", min: 3, max: 12 },
   username: { type: "string", min: 5, max: 15 },
   email: { type: "string" },
+  age: { type: "number" },
   password: { type: "string", min: 8, max: 12 },
   confirmPassword: { type: "equal", field: "password" },
   $$strict: true,
-};
+}; 
 const check = v.compile(Schema)
 module.exports = check
