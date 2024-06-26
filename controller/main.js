c416cc13c4fb813c9f5033bb54c721544103d31c
TheRockMilad <therock.milad2@outlook.com>
2024-06-27 01:20:22 +0330

    added Multer package

--- controller/main.js
+++ controller/main.js
@@ -4,6 +4,10 @@ const viewsPath = require('./../utils/viewPath')
 exports.main = (req, res) => {
   res.sendFile(path.join(viewsPath,"index.html"))
 };
+exports.upload =(req, res) => {
+  console.log(req.file);
+  res.json(req.file)
+};
 exports.about = (req, res) => {
   res.sendFile(path.join(viewsPath,"about.html"))
 };
--- router/main.js
+++ router/main.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mainRouter = express.Router();
 const mainController = require("./../controller/main");
+const multer = require("multer");
 
+const upload = multer({ dest: "uploads/" });
 // main page
-mainRouter.get("/", mainController.main);
+mainRouter
+  .route("/")
+  .get(mainController.main)
+  .post(upload.single("profile"), mainController.upload);
 mainRouter.get("/about", mainController.about);
 mainRouter.get("/content", mainController.content);
 
