9054d56eb400e843d44a158af5128ca465a9ffab
TheRockMilad <therock.milad2@outlook.com>
2024-06-27 13:29:45 +0330

    Multiple file in multer

--- router/main.js
+++ router/main.js
@@ -7,7 +7,8 @@ const uploader = require('./../middleware/multer')
 mainRouter
   .route("/")
   .get(mainController.main)
-  .post(uploader.single('profile'), mainController.upload);
+  // .post(uploader.single('profile'), mainController.upload);
+  .post(uploader.array('profile',3), mainController.upload);
 mainRouter.get("/about", mainController.about);
 mainRouter.get("/content", mainController.content);
 
