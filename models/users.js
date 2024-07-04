b16e7c5684a67df91b264a217463b954e126ab17
TheRockMilad <therock.milad2@outlook.com>
2024-07-05 00:41:12 +0330

    change UserSchema

--- models/users.js
+++ models/users.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 const timeStamp = require("mongoose-timestamp");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, minLenth: 3, maxLenth: 12 },
-  name: { type: String, required: true, minLenth: 5, maxLenth: 15 },
-  email: { type: String, required: true, unique: true },
-  age: { type: Number },
-  password: { type: String, minLenth: 8, maxLenth: 12 },
-});
-
-userSchema.plugin(timeStamp);
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, minLenth: 3, maxLenth: 12 },
+    name: { type: String, minLenth: 5, maxLenth: 15 },
+    email: { type: String, required: true, unique: true },
+    agemaile: { type: Number },
+    password: { type: String, required: true },
+    refreshToken: String,
+  },
+  { timeStamp: true }
+);
 
 const UserModel = mongoose.model("user", userSchema);
 
