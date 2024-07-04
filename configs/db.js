6aed3ed2d7c961252f33e66ae993f3cf992779c4
TheRockMilad <therock.milad2@outlook.com>
2024-07-05 00:40:42 +0330

    change server address for practice

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbUrl = "mongodb://127.0.0.1:27017/library";
+const dbUrl = "mongodb://127.0.0.1:27017/library2";
 mongoose
   .connect(dbUrl)
   .then(() => console.log("Connected to database"))
