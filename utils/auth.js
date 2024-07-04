6b7582cf3e138670a3f695ff09a497d92799adc1
TheRockMilad <therock.milad2@outlook.com>
2024-07-05 00:40:11 +0330

    Build token generator functions

---
+++ utils/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const generateAccessToken = (email) => {
+  const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "20s",
+  });
+  return token;
+};
+
+const generateRefreshToken = (email) => {
+  const token = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d",
+  });
+  return token;
+}; 
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+};
