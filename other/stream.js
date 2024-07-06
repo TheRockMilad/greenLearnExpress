de60d8209a9f2b38732354d07e8ad25d286f4524
TheRockMilad <therock.milad2@outlook.com>
2024-07-06 13:36:27 +0330

    Write Stream (buffer)

--- other/stream.js
+++ other/stream.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
-const readStream = fs.createReadStream("./other/file.txt");
+const readStream = fs.createReadStream("./other/file.txt");  // خواندن از مبدا
+const writeStream = fs.createWriteStream("./other/target.txt"); // نوشتن در مقصد
 
 readStream.on("ready", () => {
   console.log("Stream is ready to read ...");
 });
 readStream.on("data", (chunk) => {
   console.log("chunk -> ", chunk);
+  writeStream.write(chunk)
 });
 readStream.on("error", (error) => {
   console.log("error -> ", error);
